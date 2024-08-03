0882c87caf2be00a6bf31fc6a55c1f9b7d039093
Artak <artakcreativeweb8@gmail.com>
2024-08-03 21:45:55 +0400

    updated

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            abort(404);
+            return route('company.login');
         }
     }
 }
